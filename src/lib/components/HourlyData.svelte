d8d568a1e799cc1930c2df4a7fe06cd75a778e06
Zach Dodson <zdodson21@gmail.com>
2025-05-30 15:40:45 -0400

    chartjs sample if needed

--- src/lib/components/HourlyData.svelte
+++ src/lib/components/HourlyData.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
   import type { Hourly } from "../types/Hourly";
-  import Chart from 'chart.js';
+  import Chart from 'chart.js/auto'
+  import { onMount } from "svelte";
 
   export let hourlyData: Hourly = [];
   export let twelveHourTime: boolean = false;
@@ -24,11 +25,32 @@
 
     // ! Probability of Precipitation
     object.precipitation = object.precipitation * 100;
+
+  });
+
+  let chartData;
+  let chartValues = [20, 10, 5, 2, 20, 30, 45];
+  let chartLabels = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
+  let ctx;
+  let chartCanvas;
+
+  onMount(async (promise) => {
+      ctx = chartCanvas.getContext('2d');
+      let chart = new Chart(ctx, {
+        type: 'bar',
+        data: {
+            labels: chartLabels,
+            datasets: [{
+                label: 'Revenue',
+                backgroundColor: 'rgb(255, 99, 132)',
+                borderColor: 'rgb(255, 99, 132)',
+                data: chartValues
+            }]
+        }
+    });
   });
 
   console.table(hourlyData);
 </script>
 
-<div class="hourly-data">
-
-</div>
+<canvas bind:this={chartCanvas} id="myChart"></canvas>
