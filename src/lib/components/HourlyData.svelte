a3932e324bf194cb6b13a7c21b7426fd287de730
Zach Dodson <zdodson21@gmail.com>
2025-05-30 15:21:21 -0400

    data interpretation

--- src/lib/components/HourlyData.svelte
+++ src/lib/components/HourlyData.svelte
@@ -1,24 +1,28 @@
 <script lang="ts">
   import type { Hourly } from "../types/Hourly";
+  import Chart from 'chart.js';
 
   export let hourlyData: Hourly = [];
   export let twelveHourTime: boolean = false;
 
   hourlyData.forEach(object => {
-    // Time
+    // ! Time
     const CONVERTED_TIME: Date = new Date(object.time * 1000);
 
     object.time = CONVERTED_TIME.getHours();
-    if (twelveHourTime && object.time === 0) {
-      object.time = 12;
-    } else if (twelveHourTime && object.time === 12) {
-      object.halfOfDay = 'PM';
-    } else if (twelveHourTime && object.time > 12) {
-      object.time -= 12;
-      object.halfOfDay = 'PM';
+
+    if (twelveHourTime) {
+      if (object.time === 0) {
+        object.time = 12;
+      } else if (object.time === 12) {
+        object.halfOfDay = 'PM';
+      } else if (object.time > 12) {
+        object.time -= 12;
+        object.halfOfDay = 'PM';
+      }
     }
 
-    // Probability of Precipitation
+    // ! Probability of Precipitation
     object.precipitation = object.precipitation * 100;
   });
 
